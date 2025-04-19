88819dd728180ae093f392fc55ec88c651de987d
Vishal Kumar <vishal02376@gmail.com>
2025-04-20 01:06:43 +0530

    Second commits

--- routes/deepSeekQuestions.js
+++ routes/deepSeekQuestions.js
@@ -6,3 +6,22 @@ const deepSeekController = require('../controllers/deepSeekQuestions'); // Filen
 router.post('/generate', deepSeekController.generateQuestions);
 
 module.exports = router;
+
+
+
+
+
+
+
+  // Handle form submission and API call
+  // const handleGenerate = async () => {
+  //   try {
+  //     const response = await axios.post(
+  //       "http://localhost:5000/api/gemini/generate-questions",
+  //       formData
+  //     );
+  //     setGeneratedQuestions(response.data.questions); // Directly use the array
+  //   } catch (error) {
+  //     console.error("Error generating questions:", error.message);
+  //   }
+  // };
