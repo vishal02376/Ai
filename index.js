2174553eb7be5b6718bbf887c6e79bebb8fefa27
Vishal Kumar <vishal02376@gmail.com>
2025-04-19 08:04:54 +0530

    2ndcommit

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const questionRoutes = require('./routes/questionRoutes');
+// const questionRoutes = require('./routes/questionRoutes');
 const mockQuestionRoutes = require('./routes/mockQuestionRoutes');
 const pdfRoutes = require('./routes/pdfRoutes');
-const deepSeekRoutes = require('./routes/deepSeekQuestions'); 
+// const deepSeekRoutes = require('./routes/deepSeekQuestions'); 
 const geminiRoutes = require('./routes/geminiRoutes');
 
 const app = express();
@@ -13,11 +13,11 @@ app.use(cors());
 app.use(express.json());
 
 // API routes
-app.use('/api/questions', questionRoutes);  http://localhost:5000/api/questions/generate // Real API
-app.use('/api/questions', mockQuestionRoutes);  // Mock API
+// app.use('/api/questions', questionRoutes);  http://localhost:5000/api/questions/generate // Real API
+// app.use('/api/questions', mockQuestionRoutes);  // Mock API
 app.use('/api/pdf', pdfRoutes);                 // PDF generation
 
-app.use('/api/deepseek-questions', deepSeekRoutes); //http://localhost:5000/api/deepseek-questions/generate
+// app.use('/api/deepseek-questions', deepSeekRoutes); //http://localhost:5000/api/deepseek-questions/generate
 
 
 app.use('/api/gemini', geminiRoutes);
