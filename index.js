c34474cce12e7647f53f81abd57059dbfe3cdf87
Vishal Kumar <vishal02376@gmail.com>
2025-04-23 14:36:15 +0530

    3rdpromt

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ const PORT = process.env.PORT || 5000;
 //   console.log('Server running on port 5000');
 // });
 
-app.listen(5000 , ()=>{
-  console.log("Server is running on port 5000")
+app.listen(PORT, ()=>{
+  console.log("Server is Running on the Port", PORT)
 })
